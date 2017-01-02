778795214155a413f490297fca639e07dec15d9a
xoumi <xoumi712@gmail.com>
2017-01-03 04:50:53 +0530

    Previous code wasn't working for all test cases, this one is though

--- challenge_3/c/xoumi/majority.c
+++ challenge_3/c/xoumi/majority.c
@@ -7,7 +7,7 @@ void main()
     printf("Enter array of numbers: ");
     gets(array);
     for(len=0;array[len]!='\0';len++);
-    len-=1;
+    printf("%d \n",len);
 
     //Run every number from 0 to 9 and if matching, flag+=1
     //only changed one line from Challenge_2 at line20.
@@ -16,7 +16,7 @@ void main()
     
     for(i='0';i<='9';i++){
 	flag=0;
-	for(j=0;j<=len;j++)
+	for(j=0;j<len;j++)
 	    if (array[j]==i)
 		flag++;
 	if(flag>=len/2)
