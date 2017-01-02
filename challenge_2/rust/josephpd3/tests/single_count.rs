31fb55ab77417796b654407af456250c36b5f654
Joseph P DeBartola III <josephdebartola@gmail.com>
2017-01-02 10:40:22 +0000

    Add test for vector with no lonely elements

--- challenge_2/rust/josephpd3/tests/single_count.rs
+++ challenge_2/rust/josephpd3/tests/single_count.rs
@@ -7,3 +7,9 @@ fn lonely_hearts_club() {
     let v = vec!['1', '1', '2', 'a', 'b', 'b', 'b', 'c', 'c'];
     assert_eq!(get_lonely_items(&v), vec!['2', 'a']);
 }
+
+#[test]
+fn for_never_alone() {
+    let v = vec!['1', '1', '2', '2', 'a', 'a', 'b', 'b', 'b', 'c', 'c'];
+    assert_eq!(get_lonely_items(&v), vec![]);
+}
