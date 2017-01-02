03453a40aab2d3e7a955c29a3c849a810edd2e65
Julio Zanetti <zanetti.julio@gmail.com>
2017-01-02 09:21:06 -0200

    Create challenge_1
    SImple code to invert a given list using the list(), input() and .reverse() functions

---
+++ challenge_1/python/zanetti/challenge_0.py
@@ -0,0 +1,5 @@
+a=list(input("Insert the  you want to invert :")) #input to user choose the characteres to invert - in a list form
+
+print('the choosen world: ', a) 
+a.reverse() #invert the "a" list
+print('reversed: ', a)
