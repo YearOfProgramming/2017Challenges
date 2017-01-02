80f11089338a8a068f52ffda51ca86c4323e4b5f
Joseph P DeBartola III <josephdebartola@gmail.com>
2017-01-02 10:33:28 +0000

    Add solution for challenge 2

---
+++ challenge_2/rust/josephpd3/tests/single_count.rs
@@ -0,0 +1,9 @@
+extern crate josephpd3;
+
+use josephpd3::*;
+
+#[test]
+fn lonely_hearts_club() {
+    let v = vec!['1', '1', '2', 'a', 'b', 'b', 'b', 'c', 'c'];
+    assert_eq!(get_lonely_items(&v), vec!['2', 'a']);
+}
