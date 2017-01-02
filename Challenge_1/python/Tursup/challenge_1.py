42e680f0bb6859bbf75bd8153722d733036afb8e
Tursup <badgrape12@yahoo.com>
2017-01-01 20:07:49 -0500

    Revert "Reversing strings"
    This reverts commit f29de9a8e01e391995c8f383c91c5a55f60f6cb8.

--- Challenge_1/python/Tursup/challenge_1.py
+++
@@ -1,5 +0,0 @@
-string = "Reverse me." #predetermined string
-print string[::-1] #reversed
-
-string1 = raw_input("Enter something and get a suprise: ") # take user input
-print string1[::-1] #reversed user input
