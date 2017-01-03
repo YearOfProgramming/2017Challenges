a27709b9004bff6daf2bec9e0c156fdf70201055
mrfojo <oojo@appzonegroup.com>
2017-01-03 01:34:05 +0100

    challenge_1 completed in csharp

---
+++ challenge_1/csharp/fojo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fojo
+{
+    public class Challenge1
+    {
+        public static void Main(string[] args)
+        {
+            //Not using linq :)
+            var inputString = Console.ReadLine();
+            string reversedString = string.Empty;
+            for (int i = inputString.Length - 1; i >= 0; i--)
+            {
+                reversedString += inputString[i];
+            }
+            Console.WriteLine(reversedString);
+        }
+    }
+}
