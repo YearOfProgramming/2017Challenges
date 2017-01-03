65f069c82beea8e96bce780add4f6c3637a0d549
ning <ningyuan.sg@gmail.com>
2017-01-03 14:39:46 +0800

    Include check majority in first loop rather than separate loop

--- challenge_3/python/ning/challenge_3.py
+++ challenge_3/python/ning/challenge_3.py
@@ -6,10 +6,8 @@ def find_majority(sequence):
             item_counter[item] = 1
         else:
             item_counter[item] += 1
-
-    for item, item_count in item_counter.items():
-        if item_count > len(sequence) / 2:
-            return item
+            if item_counter[item] > len(sequence) / 2:
+                return item
 
 test_sequence_list = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
 
