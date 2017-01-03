5c87ff5027f10245ec2c9cc4a0fbf54b511c7a29
ning <ningyuan.sg@gmail.com>
2017-01-04 01:59:56 +0800

    Remove my solutions for challenges (/challenge_n/python/ning/*)
    I had forgotten to remove my solution in this suggestion branch.
    
    Now this branch can be merged without also merging my solutions.

--- challenge_1/python/ning/challenge_1.py
+++
@@ -1,6 +0,0 @@
-def reverse_string(original_string):
-    return original_string[::-1]
-
-original_string = str(input("Enter a string to be reversed > "))
-print(reverse_string(original_string))
-
--- challenge_3/python/ning/challenge_3.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-
-test_sequence_list = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
-print(Counter(test_sequence_list).most_common(1)[0][0])
