61d340aebbf7357e3caf587904e0d1a8b712b8d7
ning <ningyuan.sg@gmail.com>
2017-01-03 14:52:22 +0800

    Change to use collections.Counter()

--- challenge_3/python/ning/challenge_3.py
+++ challenge_3/python/ning/challenge_3.py
@@ -1,15 +1,4 @@
-def find_majority(sequence):
-    item_counter = dict()
-
-    for item in sequence:
-        if item not in item_counter:
-            item_counter[item] = 1
-        else:
-            item_counter[item] += 1
-            if item_counter[item] > len(sequence) / 2:
-                return item
+from collections import Counter
 
 test_sequence_list = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
-
-print(find_majority(test_sequence_list))
-
+print(Counter(test_sequence_list).most_common(1)[0][0])
