bf0b99ccbdc755d88dca485575e9cdd2e584193d
almyk <hansentommy92@gmail.com>
2017-01-03 11:30:45 +0900

    updated challenge #1

--- challenge_1/c/almyk/src/challenge1.c
+++ challenge_1/c/almyk/src/challenge1.c
@@ -3,14 +3,19 @@
 
 int main(void){
     char chArr[100];
+    char temp;
     int n;
 
     printf("Enter string you want to see reversed: ");
     scanf("%99[^\n]", chArr);
 
     n = strlen(chArr);
-    for(int i=n-1; i>=0; i--) printf("%c", chArr[i]);
-    printf("\n");
+    for(int i=0; i< n/2; i++){
+        temp = chArr[i];
+        chArr[i] = chArr[n - i - 1];
+        chArr[n - i - 1] = temp;
+    }
+    printf("Reversed String: %s\n", chArr);
 
     return 0;
 }
