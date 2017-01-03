a02568cdc3d68ef49cc4845db1d8cc1bbde0dded
Alex Botello <xxwingcapxx@gmail.com>
2017-01-02 22:11:59 -0600

    challenge_2 in python

---
+++ challenge_2/python/alexbotello/main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    N = int(input()) # How many items in the list?
+    array = [int(input()) for _ in range(N)] # Enter ints
+
+    print("\nThe single number is ", end='')
+    print(*[i for i in array if array.count(i) == 1], sep=' or ')
