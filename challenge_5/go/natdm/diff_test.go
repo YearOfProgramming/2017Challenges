598d84d6ddbd35caa10b76b9ac51122a17a57dcd
Nathan Hyland <nathan.hyland@gmail.com>
2017-01-03 23:44:35 -0700

    [Go] Challenge 4: Removed logs

--- challenge_5/go/natdm/diff_test.go
+++ challenge_5/go/natdm/diff_test.go
@@ -1,16 +1,10 @@
 package natdm
 
-import (
-	"log"
-	"testing"
-)
+import "testing"
 
 func TestDiff(t *testing.T) {
 	w := "1asafsd23"
 	x, n := shuffleAndAdd(w)
-	log.Println(w)
-	log.Println(string(x))
-	log.Println(string(n))
 	if diff(w, string(n)) != string(x) {
 		t.Errorf("expected %s", string(x))
 		t.Fail()
