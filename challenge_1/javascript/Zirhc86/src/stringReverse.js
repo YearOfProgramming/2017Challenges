e5459f318a2d6b395200ec9591c0cc214d9a82c2
zirhc86 <zirhc86@gmail.com>
2017-01-04 12:56:41 +1100

    Simplified reverse process

--- challenge_1/javascript/Zirhc86/src/stringReverse.js
+++ challenge_1/javascript/Zirhc86/src/stringReverse.js
@@ -1,17 +1,12 @@
 
 console.log('Running test() with no argument will log \'Example string!\' reversed.');
- 
+
 /*
  ********************************************************************
  ************************CHALLENGE ANSWER ***************************
  ********************************************************************/
 function reverseString(stringToReverse) {
-	var reversedString = "";
-	// Run throught chars of string backward, adding to reversedString
- 	for(var i = stringToReverse.length - 1; i >= 0; i--){
- 		reversedString += stringToReverse.charAt(i);
- 	}
- 	return reversedString;
+	return stringToReverse.split('').reverse().join('');
 }
 
 /********************************************************************
