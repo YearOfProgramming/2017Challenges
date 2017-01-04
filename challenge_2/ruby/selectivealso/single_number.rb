dc546639331462d24924d094539c3228b204f9eb
Luke Sweeney <luke@thesweeneys.org>
2017-01-03 22:18:03 -0600

    Moves this challenge to a method for future use (in challenge 5)

--- challenge_2/ruby/selectivealso/single_number.rb
+++ challenge_2/ruby/selectivealso/single_number.rb
@@ -1,8 +1,17 @@
 nums = [2,3,4,2,3,5,4,6,4,6,9,10,9,8,7,8,10,7]
-count = Hash.new(0)
 
-nums.each do |num|
-  count[num] += 1
+class Array
+  def find_unique
+    count = Hash.new(0)
+
+    self.each do |num|
+      count[num] += 1
+    end
+
+    return count.key(1)
+
+  end
 end
 
-puts count.key(1)
+
+puts nums.find_unique
