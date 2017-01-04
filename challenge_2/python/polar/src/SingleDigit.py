323197eb51324dd7ac44adf6daea2d3a693cf7c5
polar <wilkinstaylor71@gmail.com>
2017-01-04 12:16:32 -0500

    Challenge_2 in Python rev2

--- challenge_2/python/polar/src/SingleDigit.py
+++ challenge_2/python/polar/src/SingleDigit.py
@@ -1,6 +1,5 @@
-array  =  [2,3,4,2,3,5,4,6,4,6,9,10,9,8,7,8,10,7]
-for i in range(0,len(array)):
-    temp = array.count(i)
-    if temp == 1:
-        print(str(array[i]) + " " + "appears once in this array")
-        exit
+from collections import Counter
+dictionary = dict(Counter([2,3,4,2,3,5,4,6,4,6,9,10,9,8,7,8,10,7]))
+revdictionary = {value: key for key, value in dictionary.items()}
+print(revdictionary.get(1))
+
