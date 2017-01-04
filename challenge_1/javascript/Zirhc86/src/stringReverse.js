61cfb15b0c44c1088b18b25c48a15beb0b05e7e3
zirhc86 <zirhc86@gmail.com>
2017-01-04 12:30:59 +1100

    Added test() function to allow for testing in console. Removed unnecessary functions

--- challenge_1/javascript/Zirhc86/src/stringReverse.js
+++ challenge_1/javascript/Zirhc86/src/stringReverse.js
@@ -1,23 +1,12 @@
 
-// Sets a default string for string reverse field on page load
-var stringToReverseInput = document.getElementById("string-to-reverse");
-var defaultString = "Example string";
-stringToReverseInput.value = defaultString;
-
-var btn = document.getElementById("button--reverse-string");
-
-// Event listener for 'reverse' button, updates the output div with result from reverseString function
-// with the input value passed
-btn.addEventListener("click", function(){
-	updateOutput(reverseString(stringToReverseInput.value));
-});
- 
+console.log('Running test() with no argument will log \'Example string!\' reversed.'); 
 /*
  ********************************************************************
  ************************CHALLENGE ANSWER ***************************
  ********************************************************************/
 function reverseString(stringToReverse) {
 	var reversedString = "";
+	// Run throught chars of string backward, adding to reversedString
  	for(var i = stringToReverse.length - 1; i >= 0; i--){
  		reversedString += stringToReverse.charAt(i);
  	}
@@ -29,7 +18,12 @@ function reverseString(stringToReverse) {
 *********************************************************************/
 
 
-// Update output div with passed reverse string
-function updateOutput(reversedString) {
- 	document.getElementById("output").innerHTML = reversedString;
+// Test function. Logs default string if no string is passed when called
+function test(testString){
+	if(testString){
+		console.log(reverseString(testString));
+	}
+	else{
+		console.log(reverseString('Example string!'));
+	}
 }
