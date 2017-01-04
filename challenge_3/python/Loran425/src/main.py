cc9c846f8fa61a81404027de74e72aaddd4a28bc
Loran425 <Redseven4225>
2017-01-04 16:33:11 -0600

    Initial Code Review Fixes

--- challenge_3/python/Loran425/src/main.py
+++ challenge_3/python/Loran425/src/main.py
@@ -3,18 +3,15 @@ __author__ = "Loran425"
 
 def majority(data):
     count = {}
-    output = None  # Have a value to return if there is no majority
     for char in data:
-        # Dirty dirty dirty
-        try:
-            count[char] += 1
-        except KeyError:
+        if char not in count.keys():
             count[char] = 1
-    for x in count.keys():
-        if count[x] >= len(count) // 2:
-            output = x
-    return output
+        else:
+            count[char] += 1
+            if count[char] >= len(data) // 2:
+                return char  # Break as soon as we find a majority
 
+    return None  # Return None if there is no majority
 
 if __name__ == '__main__':
     array = [2, 2, 3, 7, 5, 7, 7, 7, 4, 7, 2, 7, 4, 5, 6, 7, 7, 8, 6, 7, 7, 8, 10, 12, 29, 30, 19, 10, 7, 7, 7, 7, 7, 7,
