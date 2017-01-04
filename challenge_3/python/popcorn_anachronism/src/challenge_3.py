f97950b0ad2916128ea2cc50ceefe55658a9c705
popcorn_anachronism <gyrfalkon@gmail.com>
2017-01-03 21:09:14 -0500

    Fixed challenge 3 no longer using .count()

--- challenge_3/python/popcorn_anachronism/src/challenge_3.py
+++ challenge_3/python/popcorn_anachronism/src/challenge_3.py
@@ -2,10 +2,16 @@ a = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7
 
 def majority_element(array):
     n = len(array)
+    array_freq = {}     #dict of frequencies of items in array
     for item in array:
-        if array.count(item) > int(n/2):
-            return(item)
-    return("No majority found. :(")
+        if item in array_freq:  #if item already present in dict, increment by 1
+            array_freq[item] = array_freq[item] + 1
+        else:                   #otherwise add item to dict with frequency of 1
+            array_freq[item] = 1
+    for item in array_freq:     #check for item with majority frequency
+        if array_freq[item] >= int(n/2):
+            return item         
+    return("No majority found. :(") #if not present, error message
 
 print(majority_element(a))
 
