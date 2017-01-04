faaf9b21b6475c8d9c0259c6436144682b353745
Femi Ojo <femiojo24@gmail.com>
2017-01-04 14:50:37 +0100

    Update Program.cs

--- challenge_1/csharp/fojo/Program.cs
+++ challenge_1/csharp/fojo/Program.cs
@@ -6,7 +6,6 @@ namespace Fojo
     {
         public static void Main(string[] args)
         {
-            //Not using linq :)
             var inputString = Console.ReadLine();
             string reversedString = string.Empty;
             for (int i = inputString.Length - 1; i >= 0; i--)
