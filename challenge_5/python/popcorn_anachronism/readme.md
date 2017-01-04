6e13a5c5f55f9254f40d2908439d71a974849ce2
popcorn_anachronism <gyrfalkon@gmail.com>
2017-01-04 08:33:21 -0500

    streamlined Challenge5 to use Counter and updated readme

--- challenge_5/python/popcorn_anachronism/readme.md
+++ challenge_5/python/popcorn_anachronism/readme.md
@@ -3,12 +3,35 @@ Given two input strings s and t, where t is a scrambled version of s with a new
 character inserted somewhere at random in the string, finds the new character.
 Collects frequencies of characters in s and t and matches them to find anomaly.
 
-To run test, either un-comment last two lines and run:
-python FindTheDifference.py
+FindTheDifference.py contains two versions of the same function,
+the older version (findTheDifference_old) uses two dicts to count frequency of characters manually;
+the newer version (findTheDifference) uses the Counter function from collections module. 
 
-or use test.py (copied from mjuiuc's folder for convenience, edited for python 3):
-python test.py
+### To run test, either run:
+	python FindTheDifference.py
 
+Time comparisons are presented below.
+
+Output:
+Test1 - Counter: 0.00027 seconds
+e
+Test1 - dicts: 0.00077 seconds
+P
+Test2 - Counter: 0.00082 seconds
+P
+Test2 - dicts: 0.00097 seconds
+None
+Test3 - Counter: 0.00071 seconds
+None
+Test3 - dicts: 0.00085 seconds
+
+
+### Or use test.py (copied from mjuiuc's folder for convenience, edited for python 3):
+	python test.py
+
+Note: This calls only the version of the function using Counter.
+
+Output:
 Runtime of Case1: 0.0 seconds
 .Runtime of Case2: 0.0 seconds
 .Runtime of Case3: 0.0 seconds
--- challenge_5/python/popcorn_anachronism/src/FindTheDifference.py
+++ challenge_5/python/popcorn_anachronism/src/FindTheDifference.py
@@ -1,5 +1,8 @@
+from collections import Counter
+import time
+
 class Solution(object):
-    def findTheDifference(self, s, t):
+    def findTheDifference_old(self, s, t):
         s_freq={} #dict for freqs of chars in s
         t_freq={} #dict for freqs of chars in t
         #populate s_freq
@@ -20,6 +23,36 @@ class Solution(object):
                 return t[j]
         return
 
+    def findTheDifference(self, s, t):
+        s_count = Counter(s)
+        t_count = Counter(t)
+        for letter in t_count:
+            if t_count[letter] != s_count[letter]:
+                return letter
+        return
+
 #standalone testing
-#Test = Solution()
-#print(Test.findTheDifference("abcd", "abcde"))
+Test = Solution()
+start1 = time.clock()
+print(Test.findTheDifference("abcd", "abcde"))
+print("Test1 - Counter: %.5f seconds" % (time.clock()-start1))
+
+start1 = time.clock()
+print(Test.findTheDifference_old("abcd", "abcde"))
+print("Test1 - dicts: %.5f seconds" % (time.clock()-start1))
+
+start2 = time.clock()
+print(Test.findTheDifference("asldfkjasdflkjasdlkfjaslkjasdlfkasjflaksjdflaksdjfslfkjalsdkfjalsdkfjasldkfjsalfkjadsldskfjsalfkdjflakfjdlaskdfjsaldkfjsaldkfjasldkfjasldkfjasldfkjasldfkjasldfkjasdflkwejfqiuvfdlskfjlasdkfjlfkdsjflsafjslkfjeoiruqwpeoruiqeioqwolkfjasldkfjalsdvkjlxkvjzldkfjsdfoiajfwoperqueriwsflkajdlfvkjxlfkjasodfiqpweoifjasldkfjlvkjxvboxkjsadofidjawleksdfjxovcibjxlfkgjaofiawjesflkdjvbxcoijalsdfkjqweoifasjdlfkjfvlbxkjlskfjalsdkfjalwdkfjalkej1231233alsdkfjsaldkfjlkjwoeifjlskjvxcnmvs,dfaksdjfaweilfjalsdkff", "asldfkjasdflkjasdlkfjaslkjasdlfkasjflaksjdflaksdjfslfkjalsdkfjalsdkfjasldkfjsalfkjadsldskfjsalfkdjflakfjdlaskdfjsaldkfjsaldkfjasldkfjasldkfjasldfkjasldfkjasldfkjasdflkwejfqiuvfdlskfjlasdkfjlfkdsjflsafjslkfjeoiruqwpeoruiqeioqwolkfjasldkfjalsdvkjlxkvjzldkfjsdfoiajfwoperqueriwsflkajdlfvkjxlfkjasodfiqpweoifjasldkfjlvkjxvboxkjsadofidjawleksdfjxovcibjxlfkgjaofiawPjesflkdjvbxcoijalsdfkjqweoifasjdlfkjfvlbxkjlskfjalsdkfjalwdkfjalkej1231233alsdkfjsaldkfjlkjwoeifjlskjvxcnmvs,dfaksdjfaweilfjalsdkff"))
+print("Test2 - Counter: %.5f seconds" % (time.clock()-start2))
+
+start2 = time.clock()
+print(Test.findTheDifference_old("asldfkjasdflkjasdlkfjaslkjasdlfkasjflaksjdflaksdjfslfkjalsdkfjalsdkfjasldkfjsalfkjadsldskfjsalfkdjflakfjdlaskdfjsaldkfjsaldkfjasldkfjasldkfjasldfkjasldfkjasldfkjasdflkwejfqiuvfdlskfjlasdkfjlfkdsjflsafjslkfjeoiruqwpeoruiqeioqwolkfjasldkfjalsdvkjlxkvjzldkfjsdfoiajfwoperqueriwsflkajdlfvkjxlfkjasodfiqpweoifjasldkfjlvkjxvboxkjsadofidjawleksdfjxovcibjxlfkgjaofiawjesflkdjvbxcoijalsdfkjqweoifasjdlfkjfvlbxkjlskfjalsdkfjalwdkfjalkej1231233alsdkfjsaldkfjlkjwoeifjlskjvxcnmvs,dfaksdjfaweilfjalsdkff", "asldfkjasdflkjasdlkfjaslkjasdlfkasjflaksjdflaksdjfslfkjalsdkfjalsdkfjasldkfjsalfkjadsldskfjsalfkdjflakfjdlaskdfjsaldkfjsaldkfjasldkfjasldkfjasldfkjasldfkjasldfkjasdflkwejfqiuvfdlskfjlasdkfjlfkdsjflsafjslkfjeoiruqwpeoruiqeioqwolkfjasldkfjalsdvkjlxkvjzldkfjsdfoiajfwoperqueriwsflkajdlfvkjxlfkjasodfiqpweoifjasldkfjlvkjxvboxkjsadofidjawleksdfjxovcibjxlfkgjaofiawPjesflkdjvbxcoijalsdfkjqweoifasjdlfkjfvlbxkjlskfjalsdkfjalwdkfjalkej1231233alsdkfjsaldkfjlkjwoeifjlskjvxcnmvs,dfaksdjfaweilfjalsdkff"))
+print("Test2 - dicts: %.5f seconds" % (time.clock()-start2))
+
+start3 = time.clock()
+print(Test.findTheDifference("", ""))
+print("Test3 - Counter: %.5f seconds" % (time.clock()-start3))
+
+start3 = time.clock()
+print(Test.findTheDifference_old("", ""))
+print("Test3 - dicts: %.5f seconds" % (time.clock()-start3))
