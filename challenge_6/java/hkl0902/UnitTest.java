ec5b33c1fe08f3a6df32c7b76ce98a06ad37b6a2
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-05 14:19:19 -0800

    Adds UnitTest class for future tests

---
+++ challenge_6/java/hkl0902/UnitTest.java
@@ -0,0 +1,12 @@
+import ucb.junit.textui;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class UnitTest {
+    public static void main (String [] ignored) {
+        textui.runClasses(UnitTest.class);
+    }
+
+}
