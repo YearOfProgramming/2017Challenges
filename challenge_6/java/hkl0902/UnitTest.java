5473b27e9fa97689a219f369a2ea28a03a521ea1
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-05 14:57:22 -0800

    Adds UnitTests

--- challenge_6/java/hkl0902/UnitTest.java
+++ challenge_6/java/hkl0902/UnitTest.java
@@ -9,4 +9,60 @@ public class UnitTest {
         textui.runClasses(UnitTest.class);
     }
 
+    @Test
+    public void testRangeToString() {
+        RangeList<Integer> rl = new RangeList<Integer>();
+
+        rl.add(1);
+        rl.add(2);
+        rl.add(3);
+        rl.add(4);
+
+        assertEquals("1->4", rl.toString());
+
+        rl.clear();
+
+        assertEquals(null, rl.toString());
+
+        rl.add(1);
+        assertEquals(null, rl.toString());
+        rl.add(2);
+        assertEquals("1->2", rl.toString());
+
+        rl.clear();
+        rl.add(5);
+        rl.add(6);
+        rl.add(7);
+        rl.add(8);
+        rl.add(9);
+        rl.add(10);
+        assertEquals("5->10", rl.toString());
+    }
+
+    @Test
+    public void testRangeCount() {
+        Ranges r = new Ranges(new int[] {1, 2, 3, 4, 5, 6});
+        assertEquals(1, r.numofRanges());
+        r = new Ranges(new int[] {1,2,3,4,8,9,10,12,13,14});
+        assertEquals(3, r.numofRanges());
+        r = new Ranges(new int[] {1,2,3,4,8,9,10,12,13,14,20,21,22,23});
+        assertEquals(4, r.numofRanges());
+    }
+
+    @Test
+    public void testChallenge() {
+        Ranges r = new Ranges(new int[] {1, 2, 3, 4, 5, 6});
+        assertArrayEquals(new String[] {"1->6"}, r.rangesString());
+        r = new Ranges(new int[] {1,2,3,4,8,9,10,12,13,14});
+        assertArrayEquals(new String[] {"1->4", "8->10", "12->14"}, r.rangesString());
+        r = new Ranges(new int[] {1,2,3,4,8,9,10,12,13,14,20,21,22,23});
+        assertArrayEquals(new String[] {"1->4", "8->10", "12->14", "20->23"}, r.rangesString());
+        r = new Ranges(new int[] {1});
+        assertArrayEquals(new String[] {}, r.rangesString());
+        r = new Ranges(new int[] {});
+        assertArrayEquals(new String[] {}, r.rangesString());
+    }
+
+
+
 }
