ed26e511978576e0edcf1a29c66c7a56da336a34
ekstrah <ekstrah.dongho@gmail.com>
2017-01-05 13:28:00 +0900

    Python Challenge 3

---
+++ challenge_3/python/ekstrah/README.md
@@ -0,0 +1,1 @@
+EKSTRAH Challenge_3 in Python 3.5
--- challenge_3/python/ekstrah/src/a.py
+++ challenge_3/python/ekstrah/src/a.py
@@ -11,8 +11,8 @@ def findingMajoList(aList): #{
 
 
 aList = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
-cList = [1, 2, 3]
+
 if __name__ == "__main__": #{
-    val = findingMajoList(cList)
+    val = findingMajoList(aList)
     print(val)
 #}
