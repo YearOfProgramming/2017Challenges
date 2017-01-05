c710b7cd032484880daa95d7b36a1e435dceab69
ekstrah <ekstrah.dongho@gmail.com>
2017-01-06 01:05:06 +0900

    Python Challenge 5

---
+++ challenge_5/python/ekstrah/src/a.py
@@ -0,0 +1,23 @@
+
+s = "ekstrah"
+t = "ekstrah5"
+
+sList = list(s)
+tList = list(t)
+for rm in sList:
+	tList.remove(rm)
+print(tList[0])
+
+
+# s = "ekstrah"
+# t = "ekstrah5"
+# sHisto = []
+# tHisto = []
+      
+# for letter in s:
+# 	sHisto.append(letter)
+# for letter in t:
+#     tHisto.append(letter)
+# for letter in t:
+#     if sHisto.count(letter) != tHisto.count(letter):
+#         print(letter)
