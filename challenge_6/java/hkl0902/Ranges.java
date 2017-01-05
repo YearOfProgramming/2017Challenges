3ba910fab00dfcff9deaca507362b9c0ede76c65
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-05 14:45:09 -0800

    Initial solution to challenge 6

---
+++ challenge_6/java/hkl0902/Ranges.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Ranges {
+    List<RangeList<Integer>> ranges;
+    int rangeCount = 0;
+    String[] rangesStr;
+
+    public Ranges(int[] arr) {
+        ranges = new ArrayList<RangeList<Integer>>();
+        if (arr.length > 0) {
+            int prev = arr[0];
+            ranges.add(new RangeList<Integer>());
+            ranges.get(rangeCount).add(prev);
+            for (int i = 1; i < arr.length; i += 1) {
+                int next = arr[i];
+                if (next == prev + 1) {
+                    ranges.get(rangeCount).add(next);
+                } else {
+                    ranges.add(new RangeList<Integer>());
+                    rangeCount += 1;
+                }
+            }
+        }
+        rangesStr = new String[rangeCount];
+        for (int i = 0; i < rangeCount; i += 1) {
+            rangesStr[i] = ranges.get(i).toString();
+        }
+    }
+
+    public int numofRanges() {
+        return rangeCount;
+    }
+
+    public String[] rangesString() {
+        return rangesStr;
+    }
+
+}
