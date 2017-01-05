2c5d0f7235c1e44089e2141691f1b6d307389585
mindm <maindi66@gmail.com>
2017-01-05 18:17:40 +0200

    Made a main() function

--- challenge_2/python/mindm/src/single.py
+++ challenge_2/python/mindm/src/single.py
@@ -4,6 +4,38 @@ import sys
 from collections import defaultdict
 
 
+def main():
+    if len(sys.argv) == 1:      # Arguments provided by piping in shell
+        args = sys.stdin.read()
+    elif len(sys.argv) == 2:    # Arguments provided as command line args
+        args = sys.argv[1]
+    else:
+        print("Error: too many arguments")
+        exit(1)
+
+    inlist = args_to_list(args)
+    test_alnum(inlist)
+
+    # Map elements to a dictionary where the key is the element and increment
+    # the value (default value is 0 for each key initially)
+    sum_dict = defaultdict(int)
+    for elem in inlist:
+        sum_dict[elem] += 1
+
+    result = []
+
+    for key, value in sum_dict.items():
+        if value == 1:
+            result.append(key)
+
+    if len(result) == 1:
+        print(result[0])
+    elif len(result) > 1:
+        print("Found more than 1 single value")
+    else:
+        print("No single values found")
+
+
 def args_to_list(arg_string):
     """ Parses argument-string to a list
     """
@@ -22,32 +54,5 @@ def test_alnum(arr):
             exit(1)
 
 
-if len(sys.argv) == 1:              # Arguments provided by piping in shell
-    args = sys.stdin.read()
-elif len(sys.argv) == 2:            # Arguments provided as command line args
-    args = sys.argv[1]
-else:
-    print("Error: too many arguments")
-    exit(1)
-
-inlist = args_to_list(args)
-test_alnum(inlist)
-
-# Map elements to a dictionary where the key is the element and increment the
-# value (default value is 0 for each key initially)
-sum_dict = defaultdict(int)
-for elem in inlist:
-    sum_dict[elem] += 1
-
-result = []
-
-for key, value in sum_dict.items():
-    if value == 1:
-        result.append(key)
-
-if len(result) == 1:
-    print(result[0])
-elif len(result) > 1:
-    print("Found more than 1 single value")
-else:
-    print("No single values found")
+if __name__ == "__main__":
+    main()
