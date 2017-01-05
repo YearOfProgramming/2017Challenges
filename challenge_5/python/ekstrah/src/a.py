dcb23c8afc83ffba93f0e1ad8f819a1f67680e39
ekstrah <ekstrah.dongho@gmail.com>
2017-01-06 01:05:51 +0900

    Python Challenge 5

--- challenge_5/python/ekstrah/src/a.py
+++ challenge_5/python/ekstrah/src/a.py
@@ -7,17 +7,3 @@ tList = list(t)
 for rm in sList:
 	tList.remove(rm)
 print(tList[0])
-
-
-# s = "ekstrah"
-# t = "ekstrah5"
-# sHisto = []
-# tHisto = []
-      
-# for letter in s:
-# 	sHisto.append(letter)
-# for letter in t:
-#     tHisto.append(letter)
-# for letter in t:
-#     if sHisto.count(letter) != tHisto.count(letter):
-#         print(letter)
