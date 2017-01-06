3101963603b2daf592e7490aa040f1490d011284
Luke Sweeney <luke@thesweeneys.org>
2017-01-05 22:29:27 -0600

    Finally solves challenge 6

--- challenge_6/ruby/selectivealso/v2/ranges.rb
+++ challenge_6/ruby/selectivealso/v2/ranges.rb
@@ -1,7 +1,7 @@
 # ints = [1,2,3,4,8,9,10,12,13,14]
 ints = [1,2,3,4,8,9,10,13,15,16,17]
 starts = []
-ranges = []
+ranges = {}
 
 ints.each do |int|
   # Find the index of the int
@@ -16,26 +16,17 @@ end
 
 x = 0
 (starts.count - 1).times do
-  ranges.push("#{ints[starts[x]]}->#{ints[starts[x + 1] - 1]}")
+  # ranges.push("#{ints[starts[x]]}->#{ints[starts[x + 1] - 1]}")
+  ranges[ints[starts[x]]] = ints[starts[x + 1] - 1]
   x = x + 1
 end
 
+ranges[ints[starts[-1]]] = ints[-1]
 
-
-ranges.push("#{ints[starts[-1]]}->#{ints[-1]}")
+ranges.each do |start, ending|
+  if start == ending
+    ranges.delete(start)
+  end
+end
 
 puts ranges.inspect
-
-# ints ---------------
-# [1, 2, 3, 4, 8, 9, 10, 12, 13, 14]
-# starts ---------------
-# [0, 4, 7]
-# ranges ---------------
-# []
-
-# puts "ints ---------------"
-# puts ints.inspect
-# puts "starts ---------------"
-# puts starts.inspect
-# puts "ranges ---------------"
-# puts ranges.inspect
