146a988512fe41db19c616c14b38369bf4edeb9b
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-05 21:34:59 -0800

    Solves Challenge_7

--- challenge_7/python/hkl0902/MissingNumber.py
+++ challenge_7/python/hkl0902/MissingNumber.py
@@ -1,3 +1,8 @@
 class MissingNumber:
-    def __init__(self):
-        
+    def __init__(self, lst):
+        self.numbers = lst
+
+    def findMissingNum(self):
+        n = len(self.numbers)
+        completeTotal = n*(n+1)/2
+        return completeTotal - sum(self.numbers)
