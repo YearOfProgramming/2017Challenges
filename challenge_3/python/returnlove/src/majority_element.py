dc69f2bbe7e02f7b8a904ef5679f5c5369e43e83
returnlove <iamsainath2@gmail.com>
2017-01-06 13:11:14 +0530

    removed unnecessary print statement in challenge 3

--- challenge_3/python/returnlove/src/majority_element.py
+++ challenge_3/python/returnlove/src/majority_element.py
@@ -8,8 +8,9 @@ import math
 
 def find_majority_element(input_list):
 	majority_condition = math.floor(len(input_list)/2)
-	print(majority_condition)	
+	# print(majority_condition)	
 	my_dict = {}
+	result = []
 	for element in input_list:
 		if element not in my_dict.keys():
 			my_dict[element] = 1
@@ -18,11 +19,12 @@ def find_majority_element(input_list):
 
 	for k in my_dict.keys():
 		if my_dict[k] > majority_condition:
-			print(k)
+			result.append(k)
+	return result
 
 def main():
 	my_list = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
-	find_majority_element(my_list)
+	print(find_majority_element(my_list))
 
 if __name__ == "__main__":
 	main()
