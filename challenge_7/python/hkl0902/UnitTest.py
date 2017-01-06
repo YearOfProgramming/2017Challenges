e2650957ea74dea2b65ac87fead90a4c95c608ce
Hyunkyu Michael Lee <hyunkyu@berkeley.edu>
2017-01-05 21:49:17 -0800

    more unit tests

--- challenge_7/python/hkl0902/UnitTest.py
+++ challenge_7/python/hkl0902/UnitTest.py
@@ -4,8 +4,8 @@ from MissingNumber import MissingNumber
 class UnitTest(unittest.TestCase):
 
     def testMissingNumber(self):
-        for i in range(5):
-            mn = MissingNumber([k for k in range(5) if k != i])
+        for i in range(100):
+            mn = MissingNumber([k for k in range(100) if k != i])
             self.assertEqual(i, mn.findMissingNum())
 
 if __name__ == '__main__':
