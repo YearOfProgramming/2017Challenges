1b066f793c6c3f8f8b1e9df2659922e2a5dbaf3a
Alex Botello <xxwingcapxx@gmail.com>
2017-01-06 17:24:13 -0600

    Refactor to remove nested for loop

--- challenge_5/python/alexbotello/FindTheDifference.py
+++ challenge_5/python/alexbotello/FindTheDifference.py
@@ -4,14 +4,13 @@ from collections import Counter
 class Solution:
     def findTheDifference(self, s, t):
 
-        s = dict(Counter(s))
-        t = dict(Counter(t))
+        s = Counter(s)
+        t = Counter(t)
 
         for key in t.keys():
-            if key not in s.keys():
-                s[key] = 0
-            if s[key] - t[key] <= -1:
+            if s[key] != t[key]:
                 return key
+
 
 
 if __name__ == '__main__':
