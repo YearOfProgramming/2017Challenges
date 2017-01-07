183745e5b206985d86a146345ce17ba8a05d299d
ningSP4 <ningyuan.sg@gmail.com>
2017-01-07 23:42:08 +0800

    Challenge #6: Add solution in python

---
+++ challenge_6/python/ning/challenge_6.py
@@ -0,0 +1,19 @@
+def ranges(input_list):
+    output_elements = list(input_list)  # copy, not assign
+    output_list = []
+
+    for integer in input_list:
+        if (integer + 1 not in input_list and
+            integer - 1 not in input_list):
+            output_elements.remove(integer)
+        elif (integer - 1 in input_list and
+              integer + 1 in input_list):
+            output_elements.remove(integer)
+
+    for i, integer in enumerate(output_elements):
+        if i % 2 == 0:
+            output_list.append('{}->{}'.format(integer, output_elements[i+1]))
+#            output_list.append(f'{integer}->{output_elements[integer+1]}')
+
+    return output_list
+
