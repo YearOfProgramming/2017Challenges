144195d0c78427b552e602db43a745ef61145794
Luke Sweeney <luke@thesweeneys.org>
2017-01-07 22:32:16 -0600

    small formatting refactors

--- challenge_3/ruby/selectivealso/majority.rb
+++ challenge_3/ruby/selectivealso/majority.rb
@@ -1,9 +1,10 @@
 nums = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
-
 counts = Hash.new(0)
 
 # For each element in nums, add 1 to the number tally
-nums.each {|num| counts[num] += 1}
+nums.each do |num|
+  counts[num] += 1
+end
 # Get the element with the highest value
 majority = counts.max_by{|k,v| v}
 
