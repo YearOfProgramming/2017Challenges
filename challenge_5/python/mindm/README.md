437458502b2ac9405438f51817e0d875311794ae
mindm <maindi66@gmail.com>
2017-01-08 17:57:07 +0200

    [Python] challenge 5

---
+++ challenge_5/python/mindm/README.md
@@ -0,0 +1,8 @@
+# Challenge_5
+## Find the Difference
+This program will find the difference in two strings!
+
+You can run it by typing the following on your command line:
+```
+$ python3 difference.py string_1 string_2
+```
---
+++ challenge_5/python/mindm/src/difference.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def find_difference(string1, string2):
+    """ Find the differing character between strings.
+        string2 must be one character longer than string1.
+    """
+    assert len(string1) + 1 == len(string2)
+    char_array = list(string2)
+    for char in string1:
+        ind = char_array.index(char)
+        char_array.pop(ind)
+
+    return char_array[0]
