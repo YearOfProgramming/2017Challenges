15d3cbd6b01d65f9d841cb8595f1f7f05aec3bc6
ningSP4 <ningyuan.sg@gmail.com>
2017-01-08 11:31:15 +0800

    Change if x in y check to use set instead of list
    Set lookup is faster

--- challenge_6/python/ning/challenge_6.py
+++ challenge_6/python/ning/challenge_6.py
@@ -1,13 +1,14 @@
 def ranges(input_list):
     output_elements = list(input_list)  # copy, not assign
+    input_lookup = set(input_list)
     output_list = []
 
     for integer in input_list:
-        if (integer + 1 not in input_list and
-            integer - 1 not in input_list):
+        if (integer + 1 not in input_lookup and
+            integer - 1 not in input_lookup):
             output_elements.remove(integer)
-        elif (integer - 1 in input_list and
-              integer + 1 in input_list):
+        elif (integer - 1 in input_lookup and
+              integer + 1 in input_lookup):
             output_elements.remove(integer)
 
     for i, integer in enumerate(output_elements):
