e7221e12a51a9f51fafaaaa3a477cb18bd86dc2f
Karan Chawla <karangc2@illinois.edu>
2017-01-09 16:41:43 -0600

    [C] Challenge 8 (Unreviewed)

---
+++ challenge_8/C/randomDeepCopy.c
@@ -0,0 +1,111 @@
+/*
+Author: karanchawla
+Random Deep Copy
+#Challenge #8
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+//node definition for the linked list
+struct node
+{
+	int data;
+	struct node* next;
+	struct node* random;
+};
+
+typedef struct node* Node;
+
+//utility function to create a node
+Node newNode(int key)
+{
+	Node temp = (struct node*) malloc(sizeof(struct node));
+
+	temp->data = key;
+	temp->next = NULL;
+	temp->random = NULL;
+
+	return temp;
+}
+
+//utility function to print the list
+void printList(Node head)
+{
+	while(head!=NULL)
+	{
+		printf("Node key: %d\t", head->data);
+		printf("Random Node pointer data: %d\n", head->random->data);
+		head = head->next;
+	}
+	return;
+}
+
+//First step is to insert a copy of each node
+//after the first node in the original list
+//Copy the random pointers
+//Separate this second list
+Node deepCopy(Node head)
+{
+	Node old;
+	Node temp = head;
+	//modify this list
+	while(temp)
+	{
+
+		Node temp2 = newNode(temp->data);
+		old = temp->next;
+		temp->next = temp2;
+		temp2->next = old;
+		temp = old;
+	}
+
+	temp = head;
+	//copy arbitrary matching
+	while(temp && temp->next)
+	{
+
+		temp->next->random = temp->random->next;
+		temp = temp->next->next;
+	}
+
+	//Separating the two lists
+	Node newNode = head->next;
+	temp = head; 
+
+	while(temp)
+	{
+		old = temp->next;
+		temp->next = old->next;
+		temp = old->next;
+		if(old->next) old->next->next;
+	}
+
+	return newNode;
+}
+
+//Driver program;
+int main(void)
+{
+	Node head = newNode(1);
+	head->next = newNode(2);
+	head->next->next = newNode(3);
+	head->next->next->next = newNode(4);
+	head->next->next->next->next = newNode(5);
+	head->next->next->next->next->next = NULL;
+	
+	// Assign random Pointers
+	head->random = head->next->next;
+	head->next->random = head->next->next->next;
+	head->next->next->random = head;
+	head->next->next->next->random = head->next;
+	head->next->next->next->next->random = head->next->next->next->next;
+
+	printList(head);
+	Node deepCopyList = deepCopy(head);
+
+	printList(deepCopyList);
+
+	return 0;
+}
