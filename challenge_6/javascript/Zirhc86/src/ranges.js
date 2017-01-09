64e4f2c32225df66c58a038dd1b409e4b081d31e
Zirhc86 <zirhc86+github@gmail.com>
2017-01-10 10:13:55 +1100

    Update ranges.js

--- challenge_6/javascript/Zirhc86/src/ranges.js
+++ challenge_6/javascript/Zirhc86/src/ranges.js
@@ -1,15 +1,14 @@
 
 
 document.getElementById('submit').addEventListener('click', function(e){
-	
-	var userArray = document.getElementById('user-array').value;
+    var userArray = document.getElementById('user-array').value;
     var lowerRange = [];
     var upperRange = [];
     var userArrayRanges = [];
 
     // Separate user string into an array, removing any
     // spaces and/or commas
-	userArray = userArray.split(/[ ,]+/g).filter(Boolean);
+    userArray = userArray.split(/[ ,]+/g).filter(Boolean);
     
     // Sort lower and upper ranges by comparing previous
     // element to current element. If the difference of
@@ -20,25 +19,25 @@ document.getElementById('submit').addEventListener('click', function(e){
     // a new element in upperRange[].
     lowerRange.push(userArray[0]);
 
-	for(var i = 1; i < userArray.length; i++) {
+    for(var i = 1; i < userArray.length; i++) {
 
-		if(userArray[i] - userArray[i-1] != 1){
+        if(userArray[i] - userArray[i-1] != 1){
 
-			upperRange.push(userArray[i-1]);
+            upperRange.push(userArray[i-1]);
 
-			lowerRange.push(userArray[i]);
-		}
-		if(i == userArray.length - 1){
+            lowerRange.push(userArray[i]);
+        }
+        if(i == userArray.length - 1){
 
-			upperRange.push(userArray[i]);
-		}
-	}
+            upperRange.push(userArray[i]);
+        }
+    }
 
     userArrayRanges = setRanges(lowerRange, upperRange);
 
     printRanges(userArrayRanges);
 
-	e.preventDefault();
+    e.preventDefault();
 	
 });
 //-----------------------------------------------------------------------------
@@ -48,40 +47,40 @@ document.getElementById('submit').addEventListener('click', function(e){
 // added to ranges[].
 function setRanges(lowerRange, upperRange) {
 	
-	var ranges = [];
+    var ranges = [];
 
-	if(lowerRange.length < 1){
+    if(lowerRange.length < 1){
 
-		return ranges;		
-	}
+        return ranges;		
+    }
 
-	for( var i = 0; i < lowerRange.length; i++) {
+    for( var i = 0; i < lowerRange.length; i++) {
 
-		if(upperRange[i] > lowerRange[i]) {
+        if(upperRange[i] > lowerRange[i]) {
 
-			ranges.push(lowerRange[i] + " -> " + upperRange[i]);
-		}
-	}
+            ranges.push(lowerRange[i] + " -> " + upperRange[i]);
+        }
+    }
 	
-	return ranges;
+    return ranges;
 }
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 function printRanges(ranges) {
 
-	if(ranges.length > 0){
+    if(ranges.length > 0){
 
-		document.getElementById('output').innerHTML = '<b>Ranges:</b>';
+        document.getElementById('output').innerHTML = '<b>Ranges:</b>';
 		
-	}
-	else {
+    }
+    else {
 
-		document.getElementById('output').innerHTML = '<b>There are no ranges</b>';
+        document.getElementById('output').innerHTML = '<b>There are no ranges</b>';
 
-	}
+    }
 
-	for(var i = 0; i < ranges.length; i++) {
+    for(var i = 0; i < ranges.length; i++) {
 
-	    document.getElementById('output').innerHTML += '<p>' + ranges[i] + '</p>';
-	}
+        document.getElementById('output').innerHTML += '<p>' + ranges[i] + '</p>';
+    }
 }
