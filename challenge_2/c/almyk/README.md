e95aedbb0929d7d8ba284a9348a2206546939093
almyk <hansentommy92@gmail.com>
2017-01-09 22:34:20 +0900

    changed approach in challenge#2

--- challenge_2/c/almyk/README.md
+++ challenge_2/c/almyk/README.md
@@ -1,7 +1,7 @@
 The program will ask you to first input the size of the array,
 then after that you need to fill in the integers.
 
-in the function findSingleInt it will first loop the array to find the maximum value,
-use that value to allocate a big enough array for mapping out how many times each integer appears.
+in the function findSingleInt it will first loop the array to create all the nodes and increment their count's value if a reoccurring value appears in the array.
 
-After it have mapped it out, it goes through the integer again to find which value only appeared once.
+After it have created all the needed nodes, it goes through the linked list to find which value only appeared once.
+If more than one such value exist, it reports so by incrementing the success value returned to main, to indicate failure.
--- challenge_2/c/almyk/src/main.c
+++ challenge_2/c/almyk/src/main.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 128
+// typedefs
+    typedef int KEY;
+    
+// structures
+typedef struct _NODE{
+    KEY key;
+    int count;
+    struct _NODE* next;
+}NODE;
 
-int findSingleInt(int *ary, int n);
+// function declarations
+int findSingleInt(int *ary, int n, int *result);
 void getInput(int **ary, int *n);
+NODE** getSetHead(void);
+NODE* createNode(KEY n);
+void searchCreateNode(KEY n);
 
 int main(void){
-    // local declarations
+// local declarations
     int *zArr;
     int result;
     int n;
+    int success;
 
 // statements
     getInput(&zArr, &n);
-    result = findSingleInt(zArr, n);
+    success = findSingleInt(zArr, n, &result);
 
     // if result is less than 0 no single integer was found
-    if(result>=0)
+    if(success==1)
         printf("Single integer is: %d\n", result);
+    else if(success > 1)
+        printf("More than one single integer was found\n");
     else printf("No single integer found\n");
 
     return 0;
@@ -29,10 +44,15 @@ void getInput(int **ary, int *n){
 
 // statements
     printf("Please enter the size of the array: ");
-    scanf("%d", n);
+    while(1){ // takes input till n > 0
+        scanf("%d", n);
+        
+        if(*n > 0) break;
+        printf("Please input a number larger than 0\n");
+    } // while
 
     // allocate memory for the array
-    *ary = malloc(sizeof(int)**n);
+    *ary = malloc(sizeof(int) * (*n));
 
     // fill the array
     printf("Please fill in the integers for the array and\n");
@@ -42,25 +62,85 @@ void getInput(int **ary, int *n){
     } // for
     
 } // getInput
-int findSingleInt(int *ary, int n){
+int findSingleInt(int *ary, int n, int *result){
 // local declarations
-    int *map;
-    int max=0;
+    NODE **head;
+    NODE *walker;
     int i;
-    int result=-1;
+    int success = 0;
     
 // statements
     for(i = 0; i < n; i++)
-        if(max < ary[i]) max = ary[i]; // find the largest value
+        searchCreateNode(ary[i]); // creating all the nodes needed to find the single int
 
-    // allocate memory for array to keep track of integers
-    map = malloc(sizeof(int)*max+1); 
-    for(i = 0; i < n; i++){
-        if(ary[i] < 0) continue; // don't accept negative values
-        map[ary[i]]++;
+    head = getSetHead();
+    for(walker = (*head)->next; walker; walker = walker->next){
+        // if more than one single int, increment success to indicate failure
+        if(walker->count == 1){ // if single int is found
+            *result = walker->key;
+            success += 1;
+        } // if
     } // for
-    for(i = 0; i <= max; i++)
-        if(map[i] == 1) result = i; // finding the single integer
 
-    return result;
+    return success;
 } // findSingleInt
+
+NODE** getSetHead(void){
+// local declarations
+    static NODE *head = NULL;
+
+// Statements
+    if(!head){ // if head is NULL, creates head
+        if(!(head = malloc(sizeof(NODE)))){
+            printf("Error when allocating memory\n");
+            exit(-1);
+        } // malloc went wrong
+        head->next = NULL;
+    }
+
+    return &head;
+} // getSetHead
+
+NODE* createNode(int n){
+// local declarations
+    NODE *new;
+
+    if(!(new = malloc(sizeof(NODE)))){
+        printf("Error when allocating memory\n");
+        exit(-1);
+    }
+    new->key = n;
+    new->count++;
+    new->next = NULL;
+
+    return new;
+} // createNode
+
+void searchCreateNode(KEY n){
+// local declarations
+    NODE **head;
+    NODE *new;
+    NODE *walker;
+
+// statements
+    head = getSetHead();
+
+    // if element is not pointing at an element create one
+    if(!((*head)->next)){
+        new = createNode(n);
+        (*head)->next = new;
+    }
+    else{
+        for(walker = (*head)->next; walker; walker = walker->next){
+            if(walker->key == n){ // if finding the node, increment it's count
+                walker->count++;
+                break;
+            }
+            else if(walker->next == NULL){ // else create the node
+                new = createNode(n);
+                walker->next = new;
+                break;
+            }
+        } // for
+    } // else
+} // searchCreateNode
