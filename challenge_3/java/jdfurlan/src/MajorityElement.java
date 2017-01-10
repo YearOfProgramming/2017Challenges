af8ca70939e362bd5af308166f328694c73af1c1
Jesse Furlan <jdfurlan@gmail.com>
2017-01-09 16:22:24 -0800

    [Java] Challenges 0-7

---
+++ challenge_3/java/jdfurlan/src/MajorityElement.java
@@ -0,0 +1,24 @@
+
+import java.util.HashMap;
+
+public class MajorityElement {
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 2, 3, 7, 5, 7, 7, 7, 4, 7, 2, 7, 4, 5, 6, 7, 7, 8, 6, 7, 7, 8, 10, 12, 29, 30, 19, 10, 7, 7,
+				7, 7, 7, 7, 7, 7, 7 };
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int n : nums) {
+			if (map.containsKey(n)) {
+				map.put(n, map.get(n) + 1);
+				if (map.get(n) > (nums.length / 2)) {
+					System.out.println(n);
+					System.exit(0);
+				}
+			} else {
+				map.put(n, 1);
+			}
+		}
+
+	}
+
+}
---
+++ challenge_6/java/jdfurlan/src/Ranges.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+
+public class Ranges {
+
+	public static void main(String[] args) {
+		int[][] arr = { { 1, 2, 3, 4, 8, 9, 10, 12, 13, 14 }, { 1, 2, 3, 4, 9, 10, 15 }, { 1, 2 }, { 1 },
+				{ 1, 2, 3, 4, 5, 8, 9, 10}, { 1, 3 }};
+		for(int[] a : arr){
+			System.out.println(findRanges(a));
+		}
+	}
+
+	private static ArrayList<String> findRanges(int[] arr) {
+		ArrayList<String> res = new ArrayList<String>();
+		int first, last;
+		for (int i = 0; i < arr.length - 1; i++) {
+			first = arr[i];
+			last = 0;
+			while (arr[i + 1] == arr[i] + 1) {
+				i++;
+				last = arr[i];
+				if (i == arr.length - 1)
+					break;
+			}
+			if (last != 0)
+				res.add(first + "->" + last);
+		}
+		return res;
+	}
+
+}
