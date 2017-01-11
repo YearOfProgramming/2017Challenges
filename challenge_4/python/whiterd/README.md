d81c93bc033e6a102ad6cc116a28475473fc6d2a
whiterd <whiterd434@gmail.com>
2017-01-10 21:53:45 -0600

    [Python] Challenge 4 (Unreviewed)

---
+++ challenge_4/python/whiterd/README.md
@@ -0,0 +1,7 @@
+# Invert Binary Tree
+
+* Take in a binary tree as a nested list.
+
+* For each level in the tree, reverse the order of the items in that level.
+
+* Output the resulting binary tree.
---
+++ challenge_4/python/whiterd/src/invert_bin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+'''
+    Input:  An object consisting of a binary tree.
+
+    Output:  The same tree with values mirrored.
+'''
+
+def mirror(node):
+    if node:
+        mirror(node.left)
+        mirror(node.right)
+        node.left, node.right = node.right, node.left
