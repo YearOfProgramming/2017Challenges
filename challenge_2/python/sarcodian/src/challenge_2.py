ddcaebaf79302b387ff99790f0c1e1451ea2a1e4
Sarcodian <sarcodian@gmail.com>
2017-01-16 15:48:28 +0300

    Added documentation and autorun

--- challenge_2/python/sarcodian/src/challenge_2.py
+++ challenge_2/python/sarcodian/src/challenge_2.py
@@ -1,4 +1,8 @@
 def pick_char(array0):
+    '''
+    array0: list, the array that is entered with one unique char
+    returns: char, a single element string or int that was unique in the array
+    '''
     unique = []
     duplicate = []
     for i in array0:
@@ -10,3 +14,5 @@ def pick_char(array0):
         else:
             unique.append(i)
     return unique[0]
+
+print(pick_char([2,'a','l',3,'l',4,'k',2,3,4,'a',6,'c',4,'m',6,'m','k',9,10,9,8,7,8,10,7]))
