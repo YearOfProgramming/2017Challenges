ca2e835e6b28adfa4e576b05aff321e839f63326
ningSP4 <ningyuan.sg@gmail.com>
2017-01-20 05:20:10 +0800

    Challenge #13: Improve solution in python

--- challenge_13/python/ning/test.py
+++ challenge_13/python/ning/test.py
@@ -1,52 +1,58 @@
 import unittest
-from challenge_13 import check_palindrome
+from challenge_13 import is_palindrome
 
 class Tests(unittest.TestCase):
     def test_given_1(self):
         self.assertEqual(
-                check_palindrome(1112111),
+                is_palindrome(1112111),
                 True
                 )
 
     def test_given_2(self):
         self.assertEqual(
-                check_palindrome(1),
+                is_palindrome(1),
                 True
                 )
 
     def test_given_3(self):
         self.assertEqual(
-                check_palindrome(59112),
+                is_palindrome(59112),
                 False
                 )
 
     def test_given_4(self):
         self.assertEqual(
-                check_palindrome(1234554321),
+                is_palindrome(1234554321),
                 True
                 )
 
     def test_given_5(self):
         self.assertEqual(
-                check_palindrome(22),
+                is_palindrome(22),
                 True
                 )
 
     def test_given_6(self):
         self.assertEqual(
-                check_palindrome(1010100101),
+                is_palindrome(1010100101),
                 False
                 )
 
     def test_given_7(self):
         self.assertEqual(
-                check_palindrome(1010110101),
+                is_palindrome(1010110101),
                 True
                 )
 
     def test_10111(self):
         self.assertEqual(
-                check_palindrome(10111),
+                is_palindrome(10111),
+                False
+                )
+
+    def test_5014(self):
+        self.assertEqual(
+                is_palindrome(5014),
                 False
                 )
 
