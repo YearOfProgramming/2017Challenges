b959630e92a1868649b69f8e53a872d09f7dfbc1
Alex Botello <xxwingcapxx@gmail.com>
2017-01-23 13:18:39 -0600

    [C++] Challenge 1

---
+++ challenge_1/cpp/alexbotello/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string input;
+
+  cout << ">: ";
+  getline(cin, input);
+
+  for (int i = input.length(); i >= 0; i--)
+  {
+    cout << input[i];
+  }
+
+  return 0;
+}
