af5fd23a023dd3025a4356d22c820163da028a6e
Karan Chawla <karangc2@illinois.edu>
2017-01-25 20:30:54 -0600

    Update challenge_16.cpp

--- challenge_16/cpp/karanchawla/src/challenge_16.cpp
+++ challenge_16/cpp/karanchawla/src/challenge_16.cpp
@@ -2,12 +2,11 @@
 // Created by karan on 1/24/17.
 //
 
-#include "challenge_16.h"
-
 //Just basic implementation of permutation formula
 #include "challenge_16.h"
 #include <string>
 
+// Iterates through the string to calculate the factorial
 int challenge_16::calculatePermutations(std::string &str)
 {
     std::string::size_type length = str.size();
