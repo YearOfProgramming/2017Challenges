7659c10188a3a159d1350961a92594d645567141
Mateusz Mielczarek <sysek@outlook.com>
2017-02-01 17:20:10 +0100

    change return to print

--- challenge_3/python/sysek/src/majority.py
+++ challenge_3/python/sysek/src/majority.py
@@ -9,7 +9,7 @@ def majority_number(db):
             dic[i] = 1
 
     maxcount = max(dic, key=lambda i: dic[i])
-    return maxcount
+    print(maxcount)
 	
 
 w = [2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]
