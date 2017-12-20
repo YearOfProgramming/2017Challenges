9994057c91d4cd2adc07fec056fc9fe634eb74cc
csuzw <rjcarrington@gmail.com>
2017-12-20 08:52:09 +0000

    [Elixir] Challenge 0 (Unreviewed) (#488)

---
+++ challenge_0/elixir/csuzw/src/mix.exs
@@ -0,0 +1,20 @@
+defmodule Hello.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :hello,
+     version: "0.1.0",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    []
+  end
+
+  defp deps do
+    [{:ex_doc, "~> 0.14.5", only: :dev}]
+  end
+end
