09fb16859dff6f2ea0c946f17742832952544275
D Davis <dfdx2@outlook.com>
2017-12-24 19:19:23 -0500

    [Python] Challenge 3 (Unreviewed) (#479)

---
+++ challenge_3/python/dfdx2/challenge_3.py
@@ -0,0 +1,6 @@
+# Count elements in an array and return the element that has been counted the most
+from statistics import mode
+
+print(mode([2,2,3,7,5,7,7,7,4,7,2,7,4,5,6,7,7,8,6,7,7,8,10,12,29,30,19,10,7,7,7,7,7,7,7,7,7]))
+
+
